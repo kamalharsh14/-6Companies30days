78d8ebe0348c3debe995fbc6a935100af0fd8a93
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-03 13:04:29 +0530

    Day 2

--- src/Day2/UglyNumbers.java
+++ src/Day2/UglyNumbers.java
@@ -2,6 +2,20 @@
 
 package Day2;
 
+import java.util.Scanner;
+
 public class UglyNumbers {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        UglyNumbers ob = new UglyNumbers();
+        int n = in.nextInt();
+        long num = ob.getNthUglyNo(n);
+        System.out.println(num);
+        in.close();
+    }
+
+    long getNthUglyNo(int n) {
+        long res = 0;
+        return res;
+    }
 }
