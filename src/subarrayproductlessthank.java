fb72db4c311260cefb6531bcd1f7160bee89dc40
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-03 08:13:20 +0530

    Day 1

--- src/subarrayproductlessthank.java
+++ src/subarrayproductlessthank.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class subarrayproductlessthank {
+public class SubarrayProductLessthanK{
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             int n = in.nextInt();
