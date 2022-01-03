28cab44abe21d549536087e4948b2c3b1cdabe61
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-03 12:44:55 +0530

    Day 2

---
+++ src/Day 2/GreatestCommonDivisorString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class GreatestCommonDivisorString{
+    public static void main(String[] args) {
+        GreatestCommonDivisorString ob = new GreatestCommonDivisorString();
+        Scanner in = new Scanner(System.in);
+        String str1 = in.nextLine();
+        String str2 = in.nextLine();
+        System.out.println(ob.gcd(str1, str2));
+        in.close();
+    }
+
+    String gcd(String str1, String str2){
+        return"";
+    }
+}
