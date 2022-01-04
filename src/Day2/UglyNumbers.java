3ae62ae35dec1e21ebcedb093632e693f479f5c8
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-04 09:58:25 +0530

    Day 2

--- src/Day2/UglyNumbers.java
+++ src/Day2/UglyNumbers.java
@@ -14,7 +14,26 @@ public class UglyNumbers {
     }
 
     long getNthUglyNo(int n) {
-        long res = 0;
-        return res;
+        long dp[] = new long[n+1];
+        int p2 = 1;
+        int p3 = 1;
+        int p5 = 1;
+
+        dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            int f2 = 2 * (int)dp[p2];
+            int f3 = 3 * (int)dp[p3];
+            int f5 = 5 * (int)dp[p5];
+
+
+            int min = Math.min(Math.min(f2, f3), f5);
+
+            if(min == f2){ p2++; }
+            if(min == f3){ p3++; }
+            if(min == f5){ p5++; }
+
+            dp[i] = (long)min;
+        }
+        return dp[n];
     }
 }
