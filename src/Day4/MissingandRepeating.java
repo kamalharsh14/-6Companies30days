f9a4b71d5884ba8417a0174cb7a369fffbcd2884
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-05 11:34:30 +0530

    Day 4

---
+++ src/Day4/MissingandRepeating.java
@@ -0,0 +1,37 @@
+//Problem Link: https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1/
+
+package Day4;
+
+import java.util.*;
+
+public class MissingandRepeating{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0 ; i < n ; i ++){
+            arr[i]  =in.nextInt();
+        }
+        int b[] = new int[n+1];
+
+        for(int i = 1 ; i <= n ; i ++){
+            b[i] = 0;
+        }
+
+        for(int i = 0 ; i < n ; i ++){
+            b[arr[i]] = b[arr[i]] + 1;
+        }
+        int res[] = new int[n];
+        for(int i = 1 ; i <= n; i ++){
+            if(b[i] > 1){
+                res[0] = i;
+            }
+            if(b[i] == 0){
+                res[1] = i;
+            }
+        }
+
+        System.out.println(res[0]+" "+res[1]);
+        in.close();
+    }
+}
