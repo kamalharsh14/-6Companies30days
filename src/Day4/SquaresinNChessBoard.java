e74492185c8d2632a9a870a713aefe7cac3d758c
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-05 11:50:37 +0530

    Day 4

---
+++ src/Day4/SquaresinNChessBoard.java
@@ -0,0 +1,22 @@
+//Problem Link: https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard1801/1#
+
+package Day4;
+import java.util.*;
+public class SquaresinNChessBoard {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        SquaresinNChessBoard ob = new SquaresinNChessBoard();
+        int n = in.nextInt();
+        System.out.println(ob.numberofsquares(n));
+        in.close();
+    }
+
+    int numberofsquares(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return (n*n) + numberofsquares(n-1);
+        }
+    }
+}
