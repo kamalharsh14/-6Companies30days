31f55c6310f8e67cbb9fe5c32e271d9e0ec3637a
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-11 21:38:40 +0530

    Day 10

--- src/Day10/RottingOrange.java
+++ src/Day10/RottingOrange.java
@@ -5,7 +5,7 @@ package Day10;
 import java.util.LinkedList;
 import java.util.*;
 
-public class RottenOrange {
+public class RottingOrange {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n=in.nextInt();
