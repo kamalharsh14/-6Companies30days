1ec8b420cd4088853d5fa62b6ed4c97e01ce1e3b
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-11 13:16:47 +0530

    Day 5

--- src/Day5/DecodetheString.java
+++ src/Day5/DecodetheString.java
@@ -12,14 +12,6 @@ public class DecodetheString {
     }
 
     String decode(String str){
-        int n = str.length();
-        String res = "";
-        Stack<Character> st = new Stack<>();
-        Stack<Character> num = new Stack<>();
-        for(int i = 0 ; i < n ; i ++){
-            char ch = str.charAt(i);
-            
-        }
         return res;
     }
 }
