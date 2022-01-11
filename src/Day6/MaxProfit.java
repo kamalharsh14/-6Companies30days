6716b965e8dd402c4ab4bd4fbeb089b1b7b0bfb9
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-11 10:58:38 +0530

    Day 6

--- src/Day6/MaxProfit.java
+++ src/Day6/MaxProfit.java
@@ -15,9 +15,27 @@ public class MaxProfit {
         System.out.println(ob.maxProfit(a, k));
         in.close();
     }
+
     public int maxProfit(int a[], int k){
-        int max = 0;
+        int n  = a.length;
+        int dp[][] = new int[k+1][n];
+
+        for(int i = 1 ; i <= k ; i ++){
+            int max = Integer.MIN_VALUE;
+
+            for(int d = 1 ; d < n ; d ++){
+                if((dp[i-1][d-1] - a[d-1]) > max){
+                    max = dp[i-1][d-1] - a[d-1];
+                }
+                if( max + a[d] > dp[i][d-1]){
+                    dp[i][d] = max + a[d];
+                }
+                else{
+                    dp[i][d] = dp[i][d-1];
+                }
+            }
+        }
         
-        return max;
+        return dp[k][n-1];
     }
 }
