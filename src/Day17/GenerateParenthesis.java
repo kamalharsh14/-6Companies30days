05ca36fa1e1397df5e61688d58e9ee534911e74b
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-21 18:15:00 +0530

    Day 17

--- src/Day17/GenerateParenthesis.java
+++ src/Day17/GenerateParenthesis.java
@@ -1,5 +1,6 @@
 package Day17;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,9 +12,34 @@ public class GenerateParenthesis {
         System.out.println(AllParenthesis(n));
         in.close();
     }
-    public static List<String> AllParenthesis(int n) 
-    {
-        return null;
+    public static List<String> AllParenthesis(int n) {
+
+        List<String> ans=new ArrayList<>();
+        solve(n,n,ans,"");
+
+        return ans;
+    }
+
+    public static void solve(int a, int b, List<String> ans, String t) {
+        if(a==0 && b==0)
+        {
+            ans.add(t);
+            return;
+        }
+
+        if(a<b)
+        {
+            if(a>=0)
+                solve(a-1,b,ans,t+'(');
+            if(b>=0)
+                solve(a,b-1,ans,t+')');
+        }
+        else if(a==b)
+        {   if(a>=0)
+            solve(a-1,b,ans,t+'(');
+        }
+        else {
+            return;
+        }
     }
 }
-
